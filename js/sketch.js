b00bd4d561138d6c1ccacc625d8b172e1322e69b
David Halliday <dhallid@verizon.net>
2014-09-07 00:23:08 -0400

    Add hover functionality, css stylin, remove <td> id

--- js/sketch.js
+++ js/sketch.js
@@ -6,8 +6,19 @@ $(document).ready(function(){
 		$('tbody').append("<tr>");
 		for (j = 1; j<= 16; j++) {
 			cellnum = 100 * i + j;
-			$('tbody').append("<td>" + cellnum + "</td>");
+			$('tbody').append("<td><div class=\"cell\">" + cellnum + "</div></td>");
 		}
 		$('tbody').append("</tr>");
 	}
-})
+});
+
+$(function(){
+	$('.cell').hover(
+		function(){ $(this).css('background-color', '#FF0000');
+					$(this).css('color', '#00FFFF');
+					},
+		function(){ $(this).css('background-color', '#00FFFF'); 
+					$(this).css('color', '#FF8000');
+					}
+	);
+});
