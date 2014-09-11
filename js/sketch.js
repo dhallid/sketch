97c306399b2af1309585b3e0c59affc7b2dcf43c
David Halliday <dhallid@verizon.net>
2014-09-10 21:09:45 -0400

    hover css simplify

--- js/sketch.js
+++ js/sketch.js
@@ -28,13 +28,7 @@ function myReset(){
 
 $(function(){
 	$('.cell').hover(
-		function(){ $(this).css('background-color', '#FF0000');
-					$(this).css('color', '#FFFFFF');
-					$(this).css('font-weight', 'bolder');
-					},
-		function(){ $(this).css('background-color', '#00FFFF'); 
-					$(this).css('color', '#FF8000');
-					$(this).css('font-weight', 'normal');
-					}
+					function(){ $(this).css({'background-color':'#FF0000', 'color':'#FFFFFF', 'font-weight':'bolder'})},
+					function(){ $(this).css({'background-color':'#00FFFF', 'color':'#FF8000', 'font-weight':'normal'})}
 	);
 });
